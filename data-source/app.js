0e0dfc22e5d3b0755868fb0ac231f3706748ba80
jchillin <cooljacobhill@gmail.com>
2019-02-16 15:14:27 -0600

    added a .gitignore

--- data-source/app.js
+++ data-source/app.js
@@ -1,7 +1,16 @@
-var http = require("http")
-var express = require("express")
-const app = express()
-const port = 3000
+var express = require("express");
+var bodyParser = require("body-parser"); //Used to parse HTTP requests
+const app = express();
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+//app.get();
+
+app.post('/', function(req,resp){
+    console.log(""+req.body.position);
+});
+
+const port = 3000;
 app.listen(port,() => console.log("Server running on port "+port))
-console.log("Server running")
+
 
