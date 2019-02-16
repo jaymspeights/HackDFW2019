a627a0e22b3f5bf2eaaa7d037764f1b9cc39b6e3
jchillin <cooljacobhill@gmail.com>
2019-02-16 16:55:26 -0600

    Added Post server compatibility

--- data-source/app.js
+++ data-source/app.js
@@ -1,16 +1,44 @@
 var express = require("express");
 var bodyParser = require("body-parser"); //Used to parse HTTP requests
+var request = require("request")
 const app = express();
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-//app.get();
+var controlIP = "http://192.168.43.104:8000/"
+var messageStorage;
+
+app.get('/',function (req,res) {
+    console.log("Get called")
+    console.log(req.body)
+});
 
 app.post('/', function(req,resp){
-    console.log(""+req.body.position);
+    console.log("Post called")
+    console.log(req.body);
+    let response = { text: 'Message Received'}
+    resp.send(response);
 });
+
+function sendUpdate(messageData){
+    request.post({
+      url: controlIp,
+      json: true,
+      body: messageData, function(error, response, body) {
+      if (!error) {
+          response.write(response.statusCode);
+      } else {
+          response.write(error);
+      }
+      response.end();
+    }
+    });
+   }
+
+function sendInfo()
+{
+    request.post()
+}
 
 const port = 3000;
 app.listen(port,() => console.log("Server running on port "+port))
-
-
