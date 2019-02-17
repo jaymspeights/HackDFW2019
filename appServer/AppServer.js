ecf85690f3bd07fe35ea7ae0c05c518bff580691
moiiiiit <43913328+moiiiiit@users.noreply.github.com>
2019-02-17 00:31:50 -0600

    changed to localhost

--- appServer/AppServer.js
+++ appServer/AppServer.js
@@ -28,7 +28,7 @@ app.use(bodyParser.urlencoded({
 //   extended: false
 // })); 
 
-const dataSourceServerIp = 'http://192.168.1.10:3000';
+const dataSourceServerIp = 'http://localhost:3000';
 
 function sendMessage(messageData, cb){
   let data;
